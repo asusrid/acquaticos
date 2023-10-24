b4f33d71fbd35be4b5cfc87d573ce12d9bf50e35
Antonio <antonio.rodriguez@ug.uchile.cl>
2023-10-24 14:27:30 +0200

    wrong file before

--- IoT_sensors/dataSendingSensor.py
+++ IoT_sensors/dataSendingSensor.py
@@ -1,20 +1,10 @@
-from flask import Flask, request
-from test import obtainNodosMealy
-from flask_cors import CORS
+import requests
 
-app = Flask(__name__)
-CORS(app)
-@app.route('/')
-def index():
-  return 'Welcome to my Flask server!'
+url = 'https://example.com/api'
+data = {'key': 'value'}
 
-@app.route('/data', methods=['POST'])
-def insert_data():
-  data = request.get_json()
-  # Do something with the data, such as inserting it into a database
-  # ...
-  print (data)
-  return jsonify({'fo': 'br', 'baz': 123})
+# Send POST request with FORM data using the data parameter
+response = requests.post(url, data=data)
 
-if __name__ == '__main__':
-  app.run(debug=True)
+# Print the response
+print(response.text)
