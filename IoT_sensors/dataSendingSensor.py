757599b96c746d99113fc4d4729ca1c0275a4159
Antonio <antonio.rodriguez@ug.uchile.cl>
2023-10-24 15:43:41 +0200

    IoT sensors

--- IoT_sensors/dataSendingSensor.py
+++ IoT_sensors/dataSendingSensor.py
@@ -9,35 +9,45 @@ sensor3Id = uuid.uuid4()
 location1= "1st location"
 location2= "2nd location"
 location3= "3rd location"
+unity = "L"
 t = 2
+value1 = 2000000
+value2 = 2000000
+value3 = 2000000
 url = 'https://example.com/data'
 
 
 # Send POST request with FORM data using the data parameter
 
 while True:
-  value1 = random.randint(10, 15)
   data = {"Id": str(sensor1Id),
           "Location" : location1,
+          "Unit" : unity,
           "Value": value1}
   response = requests.post(url, data=data)
   print(response.text)
+  value1 += random.randint(1900, 2150)
   time.sleep(t)
 
-  value2 = random.randint(10, 15)
+
+  
   data = {"Id": str(sensor2Id),
           "Location" : location2,
+          "Unit" : unity,
           "Value": value2}
   response = requests.post(url, data=data)
   print(response.text)
+  value2 += random.randint(1900, 2150)
   time.sleep(t)
 
-  value3 = random.randint(10, 15)
+
   data = {"Id": str(sensor3Id),
           "Location" : location3,
+          "Unit" : unity,
           "Value": value3}
   response = requests.post(url, data=data)
   print(response.text)
+  value2 += random.randint(1900, 2150)
   time.sleep(t)
 # Print the response
 
