fcc7106c37b03ca6ff624ab4c118925a434ff647
Bernardo Dantas <bernardoniteroi@gmail.com>
2023-10-24 15:46:57 +0200

    Add API

---
+++ api/models/Data.js
@@ -0,0 +1,11 @@
+/*export */ class Data {
+    constructor(id, companyName, companyId) {
+        this.id = id;
+        this.companyName = companyName;
+        this.companyId = companyId;
+    }
+}
+
+module.exports = {
+    Data: Data
+};
