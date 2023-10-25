578533353e82c02b28a1db8aaf645666f446c2f6
asusrid <alexsurid@gmail.com>
2023-10-25 15:29:55 +0200

    new contracts

--- contracts/AcquaToken.sol
+++ contracts/AcquaToken.sol
@@ -4,16 +4,22 @@ pragma solidity >=0.7.0 <0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/solc-0.7/contracts/token/ERC20/ERC20.sol";
 
 contract AcquaToken is ERC20 {
-    mapping(address => uint) public balanceAcqua;
-
-    constructor() ERC20("AcquaToken", "ACQ") {}
+    constructor() ERC20("AcquaToken", "ACQUA") {}
 
     function mint(address _to, uint _amount) external {
         _mint(_to, _amount);
-        balanceAcqua[_to] += 1;
     }
 
     function burn(address _to, uint _amount) external {
         _burn(_to, _amount);
+    }
+
+    function transfer(
+        uint256 _amount,
+        address _sender,
+        address _recipient
+    ) external returns (bool) {
+        _transfer(_sender, _recipient, _amount);
+        return true;
     }
 }
