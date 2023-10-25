3f4aec465c9c24b5d838bd0a06707556d3b7c0d8
asusrid <alexsurid@gmail.com>
2023-10-25 16:47:52 +0200

    adding

--- frontend-v2/react-ethers-erc20/src/App.js
+++ frontend-v2/react-ethers-erc20/src/App.js
@@ -1,45 +1,60 @@
 import React, { useState, useEffect } from 'react';
-
-import erc20Abi from './erc20abi';
-import { ethers } from "ethers";
-
+import { ethers } from 'ethers';
+import {
+  ACQUATOKEN_CONTRACT_ADDRESS,
+  ACQUADASHBOARD_CONTRACT_ADDRESS,
+  ACQUATOKEN_ABI,
+  ACQUADASHBOARD_ABI,
+} from './constants/constants';
+import SensorTable from './SensorTable';
 
 function App() {
   const [provider, setProvider] = useState(null);
-  const [contract, setContract] = useState(null);
+  const [acquaToken, setAcquaToken] = useState(null);
+  const [acquaDashboard, setAcquaDashboard] = useState(null);
   const [balance, setBalance] = useState(0);
   const [totalSupply, setTotalSupply] = useState(0);
-  const [recipient, setRecipient] = useState('');
-  const [amount, setAmount] = useState('');
-
-  const contractAddress = '0x9AeFc71ca54a7752CC3A00362b7F0CAa8E69b959';  // Replace with your contract address
+  // const [recipient, setRecipient] = useState('');
+  // const [amount, setAmount] = useState('');
 
   useEffect(() => {
     async function initialize() {
       if (window.ethereum != null) {
-        const ethersProvider = new ethers.BrowserProvider(window.ethereum)
+        const ethersProvider = new ethers.BrowserProvider(window.ethereum);
         const signer = await ethersProvider.getSigner();
 
         window.ethereum.request({
-          method: "wallet_addEthereumChain",
-          params: [{
-            chainId: "0x33",
-            rpcUrls: ["https://erpc.apothem.network"],
-            chainName: "Apothem Testnet",
-            nativeCurrency: {
-              name: "TXDC",
-              symbol: "TXDC",
-              decimals: 18
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: '0x33',
+              rpcUrls: ['https://erpc.apothem.network'],
+              chainName: 'Apothem Testnet',
+              nativeCurrency: {
+                name: 'TXDC',
+                symbol: 'TXDC',
+                decimals: 18,
+              },
+              blockExplorerUrls: ['https://apothem.xinfinscan.com/'],
             },
-            blockExplorerUrls: ["https://apothem.xinfinscan.com/"]
-          }]
+          ],
         });
 
-        const erc20Contract = new ethers.Contract(contractAddress, erc20Abi, signer);
+        const acquaTokenContract = new ethers.Contract(
+          ACQUATOKEN_CONTRACT_ADDRESS,
+          ACQUATOKEN_ABI,
+          signer
+        );
+        const acquaDashboardContract = new ethers.Contract(
+          ACQUADASHBOARD_CONTRACT_ADDRESS,
+          ACQUADASHBOARD_ABI,
+          signer
+        );
         setProvider(ethersProvider);
-        setContract(erc20Contract);
+        setAcquaToken(acquaTokenContract);
+        setAcquaDashboard(acquaDashboardContract);
       } else {
-        console.error("Please install MetaMask!");
+        console.error('Please install MetaMask!');
       }
     }
     initialize();
@@ -47,49 +62,38 @@ function App() {
 
   useEffect(() => {
     async function fetchBalance() {
-      if (contract) {
-        console.log('fetching balance...')
-        const ethersProvider = new ethers.BrowserProvider(window.ethereum)
+      if (acquaToken) {
+        console.log('fetching balance...');
+        const ethersProvider = new ethers.BrowserProvider(window.ethereum);
         const currentAddress = (await ethersProvider.getSigner()).getAddress();
-        const userBalance = await contract.balanceOf(currentAddress);
-        console.log("Balance: " + userBalance);
+        const userBalance = await acquaToken.balanceOf(currentAddress);
+        const test = await acquaDashboard.pullDataByCompany(currentAddress);
+        console.log('Balance: ' + userBalance);
         setBalance(userBalance);
-        const supply = await contract.totalSupply();
-        console.log("Supply: " + supply);
+        const supply = await acquaToken.totalSupply();
+        console.log('Supply: ' + supply);
         setTotalSupply(supply);
       }
     }
     fetchBalance();
-  }, [contract, provider]);
+  }, [acquaToken, provider]);
 
-  const handleTransfer = async () => {
-    if (contract) {
-      const weiAmount = amount;
-      await contract.transfer(recipient, weiAmount);
-    }
-  };
+  // const handleTransfer = async () => {
+  //   if (acquaToken) {
+  //     const weiAmount = amount;
+  //     await acquaToken.transfer(recipient, weiAmount);
+  //   }
+  // };
 
   return (
-      <div className="App">
-        <h1>AcquaCoin</h1>
-        <div>
-          <h2>Your Balance: {balance}</h2>
-          <h2>Total Supply: {totalSupply}</h2>
-        </div>
-        {/*<div>*/}
-        {/*  <input*/}
-        {/*      value={recipient}*/}
-        {/*      onChange={e => setRecipient(e.target.value)}*/}
-        {/*      placeholder="Recipient Address"*/}
-        {/*  />*/}
-        {/*  <input*/}
-        {/*      value={amount}*/}
-        {/*      onChange={e => setAmount(e.target.value)}*/}
-        {/*      placeholder="Amount"*/}
-        {/*  />*/}
-        {/*  <button onClick={handleTransfer}>Transfer</button>*/}
-        {/*</div>*/}
+    <div className="App">
+      <h1>AcquaCoin</h1>
+      <div>
+        <h2>Your Balance: {balance}</h2>
+        <h2>Total Supply: {totalSupply}</h2>
       </div>
+      <SensorTable contract={acquaDashboard} />
+    </div>
   );
 }
 
---
+++ frontend-v2/react-ethers-erc20/src/SensorTable.js
@@ -0,0 +1,4 @@
+const Table = ({ contract }) => {
+  // console.log(contract.pullData());
+};
+export default Table;
